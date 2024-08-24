e0e8826ede70b47ac801765fc27f4e34178ad25c
factoid233 <szlvps@163.com>
2024-08-24 21:36:37 +0800

    删除规则变更大于150次才删除

--- cmd/brush/strategy/strategy.go
+++ cmd/brush/strategy/strategy.go
@@ -248,7 +248,7 @@ func Decide(clientStatus *client.Status, clientTorrents []*client.Torrent, siteT
 				})
 				clientTorrentsMap[torrent.InfoHash].DeleteCandidateFlag = true
 			}
-		} else if slowCount := torrentRecordManager.GetSlowTorrentCountByHash(torrent.InfoHash); slowCount > 50 {
+		} else if slowCount := torrentRecordManager.GetSlowTorrentCountByHash(torrent.InfoHash); slowCount > 150 {
 			// 大于指定次数进行删除
 			deleteCandidateTorrents = append(deleteCandidateTorrents, candidateClientTorrentStruct{
 				InfoHash:    torrent.InfoHash,
